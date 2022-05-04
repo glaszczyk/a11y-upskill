82fa7cadfa0f587752850db44537e87ba3244ea1
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-04 22:24:18 +0200

    Change folder structure. Extract Navbar, Main and Footer

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import {Test} from "../components/Test/Test";
+import {Navbar} from "../layouts/Navbar/Navbar";
+import {Footer} from "../layouts/Footer/Footer";
+import {Main} from "../layouts/Main/Main";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>The Insurer. Travel insurance company</title>
+        <meta name="description" content="Services for travel agencies" />
+        <link rel="icon" href="/public/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <Main>
+          <Test />
+      </Main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
