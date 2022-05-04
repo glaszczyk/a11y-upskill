5a82f0899eb3b8c90242c2527f376f63928ccab5
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-04 23:20:55 +0200

    Switch to SCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import {Test} from "../components/Test/Test";
 import {Navbar} from "../layouts/Navbar/Navbar";
 import {Footer} from "../layouts/Footer/Footer";
