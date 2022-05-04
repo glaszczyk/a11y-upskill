ebb16e6e27bb227cab6afd11eaedb498d973d8be
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-04 22:36:32 +0200

    Feature/change folder structure (#2)
    * Change folder structure. Extract Navbar, Main and Footer
    
    * Remove unnecessary .next folder

---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,9 @@
+import  styles from './Test.module.css'
+export const Test = () => {
+  return (
+      <div>
+      <h1 className={styles.header}>Test</h1>
+      <p>This is test</p>
+      </div>
+  )
+}
