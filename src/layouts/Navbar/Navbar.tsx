26ce92027d9f19c0cf3070c941960073f2713fc0
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-06 14:51:06 +0200

    Add basic navbar content and styles

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,3 +1,15 @@
+import styles from './Navbar.module.scss'
+
 export const Navbar = () => {
-  return <nav>Logo</nav>
+  return (
+    <nav className={styles.container}>
+      <p className={styles.logo}>Logo</p>
+      <ul className={styles.menu}>
+        <li className={styles.menuItem}>Insurance</li>
+        <li className={styles.menuItem}>About us</li>
+        <li className={styles.menuItem}>Claim report</li>
+        <li className={styles.menuItem}>Contact</li>
+      </ul>
+    </nav>
+  )
 }
