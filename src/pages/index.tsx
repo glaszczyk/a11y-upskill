ea2bd1c99d7e5066e4c22644c5c402fe9f4f6bac
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-06 14:49:36 +0200

    Feature/add page container (#5)
    * Add PageContainer component
    
    * Add media queries mixin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,20 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+
 import { Test } from '../components/Test/Test'
 import { Navbar } from '../layouts/Navbar/Navbar'
 import { Footer } from '../layouts/Footer/Footer'
 import { Main } from '../layouts/Main/Main'
+import { PageContainer } from '../components/PageContainer/PageContainer'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>The Insurer. Travel insurance company</title>
-        <meta name="description" content="Services for travel agencies" />
-        <link rel="icon" href="/public/favicon.ico" />
-      </Head>
-
+    <PageContainer>
       <Navbar />
-
       <Main>
         <Test />
       </Main>
-
       <Footer />
-    </div>
+    </PageContainer>
   )
 }
 
