a454f9306def60d7c6f7b10e48f461e340d6ad1b
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-10 22:50:51 +0200

    Feature/update navbar and footer (#6)
    * Add basic navbar content and styles
    
    * Add logo
    
    * Update navbar without adding route files
    
    * Update main
    
    * Update footer. Add first route file

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,19 +1,110 @@
 import styles from './Footer.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '../../../public/TheInsurerLogo.svg'
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
+      <Link href="/">
+        <a>
+          <Image
+            src={Logo}
+            className={styles.logo}
+            alt=""
+            width={200}
+            height={43}
+          />
+        </a>
+      </Link>
+      <div className={styles.menuContainer}>
+        <ol className={styles.menu}>
+          <li className={styles.menuItem}>
+            <Link href="/about-us">
+              <a rel="noopener noreferrer">About us</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/newsroom">
+              <a rel="noopener noreferrer">Newsroom</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/careers">
+              <a rel="noopener noreferrer">Careers</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/our-story">
+              <a rel="noopener noreferrer">Our story</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/customer-stories">
+              <a rel="noopener noreferrer">Customer Stories</a>
+            </Link>
+          </li>
+        </ol>
+        <ol className={styles.menu}>
+          <li className={styles.menuItem}>
+            <Link href="/products">
+              <a rel="noopener noreferrer">Products</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/vacation-holiday">
+              <a rel="noopener noreferrer">Vacation / Holiday</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/student-scholar">
+              <a rel="noopener noreferrer">Student / Scholar</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/mission">
+              <a rel="noopener noreferrer">Mission</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/marine-captain-crew">
+              <a rel="noopener noreferrer">Marine Captain / Crew</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/employer-business-traveler">
+              <a rel="noopener noreferrer">Employer / Business Traveler</a>
+            </Link>
+          </li>
+        </ol>
+        <ol className={styles.menu}>
+          <li className={styles.menuItem}>
+            <Link href="/members">
+              <a rel="noopener noreferrer">Members</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/forms-library">
+              <a rel="noopener noreferrer">Forms Library</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/find-a-doctor">
+              <a rel="noopener noreferrer">Find a Doctor</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/renew-policy">
+              <a rel="noopener noreferrer">Renew Policy</a>
+            </Link>
+          </li>
+          <li className={styles.menuItem}>
+            <Link href="/claims-center">
+              <a rel="noopener noreferrer">Claims Center</a>
+            </Link>
+          </li>
+        </ol>
+      </div>
     </footer>
   )
 }
--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,3 +1,45 @@
+import Image from 'next/image'
+
+import styles from './Navbar.module.scss'
+import Logo from '../../../public/TheInsurerLogo.svg'
+import Link from 'next/link'
+
 export const Navbar = () => {
-  return <nav>Logo</nav>
+  return (
+    <nav className={styles.container}>
+      <Link href="/">
+        <a>
+          <Image
+            src={Logo}
+            className={styles.logo}
+            alt="Return to main page"
+            width={200}
+            height={43}
+          />
+        </a>
+      </Link>
+      <ul className={styles.menu} role="menubar">
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/insurance">
+            <a>Insurance</a>
+          </Link>
+        </li>
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/about-us">
+            <a>About us</a>
+          </Link>
+        </li>
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/claim-report">
+            <a>Claim report</a>
+          </Link>
+        </li>
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
