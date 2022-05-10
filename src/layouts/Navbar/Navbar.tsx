70f093f7cbf5411b09bdc2bc6f3ef048ddc3d748
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-10 22:49:33 +0200

    Update navbar without adding route files

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,14 +1,44 @@
+import Image from 'next/image'
+
 import styles from './Navbar.module.scss'
+import Logo from '../../../public/TheInsurerLogo.svg'
+import Link from 'next/link'
 
 export const Navbar = () => {
   return (
     <nav className={styles.container}>
-      <p className={styles.logo}>Logo</p>
-      <ul className={styles.menu}>
-        <li className={styles.menuItem}>Insurance</li>
-        <li className={styles.menuItem}>About us</li>
-        <li className={styles.menuItem}>Claim report</li>
-        <li className={styles.menuItem}>Contact</li>
+      <Link href="/">
+        <a>
+          <Image
+            src={Logo}
+            className={styles.logo}
+            alt="Return to main page"
+            width={200}
+            height={43}
+          />
+        </a>
+      </Link>
+      <ul className={styles.menu} role="menubar">
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/insurance">
+            <a>Insurance</a>
+          </Link>
+        </li>
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/about-us">
+            <a>About us</a>
+          </Link>
+        </li>
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/claim-report">
+            <a>Claim report</a>
+          </Link>
+        </li>
+        <li role="menuitem" className={styles.menuItem}>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
       </ul>
     </nav>
   )
