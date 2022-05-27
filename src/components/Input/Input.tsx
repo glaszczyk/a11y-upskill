ea5aca842735edab26298c7ce7002555445f7ccf
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-27 16:46:46 +0200

    Add date type to input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,15 @@
 import { FormEvent } from 'react'
 
 import styles from './Input.module.scss'
+import classnames from 'classnames'
 
 type InputPropTypes = {
   name: string
   label: string
-  type: 'text' | 'email'
+  type: 'text' | 'email' | 'date'
   value: string
   onChange: (value: string) => void
+  className?: string
 }
 export const Input = ({
   type = 'text',
@@ -15,17 +17,19 @@ export const Input = ({
   label,
   name,
   value,
+  className,
 }: InputPropTypes) => {
   const handleChange = (e: FormEvent<HTMLInputElement>) =>
     onChange(e.currentTarget.value)
 
+  const classNames = classnames(styles.input, className)
   return (
     <div className={styles.inputWrapper}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
       <input
-        className={styles.input}
+        className={classNames}
         id={name}
         name={name}
         type={type}
