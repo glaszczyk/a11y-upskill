038a00c3d8639717ea1fa1f79280b26194af7989
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-05-29 21:38:42 +0200

    Add link and pages to blogpost and contact (#15)

---
+++ src/pages/trends-predictions.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+
+import { Navbar } from '../layouts/Navbar'
+import { Footer } from '../layouts/Footer'
+import { Main } from '../layouts/Main'
+import { PageContainer } from '../components/PageContainer'
+
+const Home: NextPage = () => {
+  return (
+    <PageContainer>
+      <Navbar />
+      <Main>
+        <h1>Trends & Predictions</h1>
+      </Main>
+      <Footer />
+    </PageContainer>
+  )
+}
+
+export default Home
