c9d0922ec0e3ccf7cce15dbbc2cac15a8ff6633b
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-06-01 08:18:30 +0200

    Add language (#21)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US', 'fr', 'nl-NL'],
+    defaultLocale: 'en-US',
+  },
 }
 
 module.exports = {
